92e77eb94681ad11ba580edee9141cea0d3320ec
JD <zelenuj@gmail.com>
2021-10-05 14:16:20 +0300

    CU-1na9m4g CU-1na9mg4[completed] Перевірка існування і створення таблиці "modification"

--- install.php
+++ install.php
@@ -1,12 +1,37 @@
 <?php
-// Перевірка на існування таблиць
-$check = $this->db->query("SELECT * "
+// v3.0.3.7.2
+// Check and create tables if not exits
+$check_modification = $this->db->query("SELECT * "
+	. " FROM information_schema.tables "
+	. " WHERE table_schema = '" . DB_DATABASE . "'"
+	. " AND table_name = '" . DB_PREFIX . "modification'"
+	. " LIMIT 1;"
+	)->num_rows;
+if (!$check_modification) {
+	$modification_table_sql = "CREATE TABLE `". DB_PREFIX . "modification` ("
+		. " `modification_id` INT(11) NOT NULL AUTO_INCREMENT,"
+		. " `extension_install_id` INT(11) NOT NULL,"
+		. " `name` VARCHAR(64) NOT NULL COLLATE 'utf8_general_ci',"
+		. " `code` VARCHAR(64) NOT NULL COLLATE 'utf8_general_ci',"
+		. " `author` VARCHAR(64) NOT NULL COLLATE 'utf8_general_ci',"
+		. " `version` VARCHAR(32) NOT NULL COLLATE 'utf8_general_ci',"
+		. " `link` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',"
+		. " `xml` MEDIUMTEXT NOT NULL COLLATE 'utf8_general_ci',"
+		. " `status` TINYINT(1) NOT NULL,"
+		. " `date_added` DATETIME NOT NULL,"
+		. " PRIMARY KEY (`modification_id`) USING BTREE"
+		. ")"
+		. "	COLLATE='utf8_general_ci'"
+		. " ENGINE=MyISAM;";
+	$this->db->query($modification_table_sql);
+}
+$check_modification_backup = $this->db->query("SELECT * "
 	. " FROM information_schema.tables "
 	. " WHERE table_schema = '" . DB_DATABASE . "'"
 	. " AND table_name = '" . DB_PREFIX . "modification_backup'"
 	. " LIMIT 1;"
 	)->num_rows;
-if (!$check) {
+if (!$check_modification_backup) {
 	$table_sql = "CREATE TABLE `" . DB_PREFIX . "modification_backup` ("
 		. " `backup_id` INT(11) NOT NULL AUTO_INCREMENT,"
 		. " `modification_id` INT(11) NOT NULL,"
